686c107d8a9dcab5d850f29baf05a76c8db4dc7b
Samuele Calò <samuelereus99@gmail.com>
2022-08-22 17:32:52 +0200

    css implementation signuppage

--- src/pages/signuppage.js
+++ src/pages/signuppage.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Servermessage from '../components/servermessage'
+import ThemeProvider from 'react-bootstrap/ThemeProvider'
+import { Col, Container, Row } from 'react-bootstrap';
 
-const baseURI='http://localhost:8080/api';
+const baseURI = 'http://localhost:8080/api';
 
 class Signuppage extends Component {
     constructor(props) {
@@ -43,62 +45,96 @@ class Signuppage extends Component {
                 elicitationId: this.state.elicitationId
             })
         }).then((res) => {
-            if(res.status === 200) {
-                this.setState({serverSuccess: true, serverMessage: "User registered successfully"});
+            if (res.status === 200) {
+                this.setState({ serverSuccess: true, serverMessage: "User registered successfully" });
             } else {
                 res.json().then((document) => {
-                    this.setState({serverSuccess: false, serverMessage: document.message});
+                    this.setState({ serverSuccess: false, serverMessage: document.message });
                 });
             }
         }).catch((error) => {
-            this.setState({serverSuccess: false, serverMessage: error.message});
+            this.setState({ serverSuccess: false, serverMessage: error.message });
         });
     }
 
     handleChange = (e) => {
-        this.setState({[e.target.name]: e.target.value});
+        this.setState({ [e.target.name]: e.target.value });
     }
 
     render() {
         return (
             <>
-                <h1>Signup</h1>
-                <Servermessage serverSuccess={this.state.serverSuccess} serverMessage={this.state.serverMessage} />
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Group className='mb-3'>
-                        <Form.Label>
-                            Username
-                        </Form.Label>
-                        <Form.Control type='text' name='username' value={this.state.username} onChange={this.handleChange}/>
-                        <Form.Label>
-                            Firstname
-                        </Form.Label>
-                        <Form.Control type='text' name='firstname' value={this.state.firstname} onChange={this.handleChange}/>
-                        <Form.Label>
-                            Lastname
-                        </Form.Label>
-                        <Form.Control type='text' name='lastname' value={this.state.lastname} onChange={this.handleChange}/>
-                        <Form.Label>
-                            Gender
-                        </Form.Label>
-                        <Form.Select name='gender' value={this.state.gender} onChange={this.handleChange}>
-                            <option>Select your gender</option>
-                            <option value={'m'}>Male</option>
-                            <option value={'f'}>Female</option>
-                        </Form.Select>
-                        <Form.Label>
-                            Age
-                        </Form.Label>
-                        <Form.Control type='number' name='age' value={this.state.age} onChange={this.handleChange}/>
-                        <Form.Label>
-                            ElicitationId
-                        </Form.Label>
-                        <Form.Control type='number' name='elicitationId' value={this.state.elicitationId} onChange={this.handleChange}/>
-                    </Form.Group>
-                    <Button variant='primary' type='submit'>
-                        Signup
-                    </Button>
-                </Form>
+                <Container>
+                    <Row className="justify-content-center">
+                        <Col lg="2">
+                            <h1>Signup</h1>
+                        </Col>
+                    </Row>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Group>
+                            <Row className="justify-content-center">
+                                <Col lg="6">
+                                    <Form.Label>
+                                        Username
+                                    </Form.Label>
+                                    <Form.Control type='text' name='username' value={this.state.username} onChange={this.handleChange} />
+                                </Col>
+                            </Row>
+                            <Row className="justify-content-center">
+                                <Col lg="6">
+                                    <Form.Label>
+                                        Firstname
+                                    </Form.Label>
+                                    <Form.Control type='text' name='firstname' value={this.state.firstname} onChange={this.handleChange} />
+                                </Col>
+                            </Row>
+                            <Row className="justify-content-center">
+                                <Col lg="6">
+                                    <Form.Label>
+                                        Lastname
+                                    </Form.Label>
+                                    <Form.Control type='text' name='lastname' value={this.state.lastname} onChange={this.handleChange} />
+                                </Col>
+                            </Row>
+                            <Row className="justify-content-center">
+                                <Col lg="6">
+                                    <Form.Label>
+                                        Gender
+                                    </Form.Label>
+                                    <Form.Select name='gender' value={this.state.gender} onChange={this.handleChange}>
+                                        <option>Select your gender</option>
+                                        <option value={'m'}>Male</option>
+                                        <option value={'f'}>Female</option>
+                                    </Form.Select>
+                                </Col>
+                            </Row>
+                            <Row className="justify-content-center">
+                                <Col lg="6">
+                                    <Form.Label>
+                                        Age
+                                    </Form.Label>
+                                    <Form.Control type='number' name='age' value={this.state.age} onChange={this.handleChange} />
+                                </Col>
+                            </Row>
+                            <Row className="justify-content-center">
+                                <Col lg="6">
+                                    <Form.Label>
+                                        ElicitationId
+                                    </Form.Label>
+                                    <Form.Control type='number' name='elicitationId' value={this.state.elicitationId} onChange={this.handleChange} />
+                                </Col>
+                            </Row>
+                        </Form.Group>
+                        <Row className="justify-content-center" lg="6" style={{marginTop:"10px"}}>
+                            
+                                <Button variant='primary' type='submit'>
+                                    Signup
+                                </Button>
+                            
+                        </Row>
+                    </Form>
+                    <Servermessage serverSuccess={this.state.serverSuccess} serverMessage={this.state.serverMessage} />
+                </Container>
             </>
         );
     }
